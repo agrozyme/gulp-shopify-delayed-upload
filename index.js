4a6eef0dfcf0eda411938aad591c7f40c74f5b90
agrozyme <agrozyme@gmail.com>
2016-05-17 15:24:01 +0800

    refactory code use $ to get variables

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
-var through = require('through2');
-var gutil = require('gulp-util');
-var inquirer = require('inquirer');
-var path = require('path');
-var opn = require('open');
-var isBinaryFile = require('isbinaryfile');
-var ShopifyApi = require('shopify-api');
-var PluginError = gutil.PluginError;
-var shopify = {};
-var shopifyAPI;
-
 var PLUGIN_NAME = 'gulp-shopify-upload';
+var shopifyAPI;
+var shopify = {};
+
+var $ = {
+  through: require('through2'),
+  gutil: require('gulp-util'),
+  inquirer: require('inquirer'),
+  path: require('path'),
+  open: require('open'),
+  isBinaryFile: require('isbinaryfile'),
+  ShopifyApi: require('shopify-api')
+};
 
 // Set up shopify API information
 shopify._api = false;
@@ -21,16 +22,10 @@ shopify._basePath = false;
  *
  * @return {ShopifyApi}
  */
-shopify._getApi = function (apiKey, password, host) {
+shopify._getApi = function(apiKey, password, host) {
   if (!shopify._api) {
-    var opts = {
-      auth: apiKey + ':' + password,
-      host: host,
-      port: '443',
-      timeout: 120000
-    };
-
-    shopify._api = new ShopifyApi(opts);
+    var opts = {auth: apiKey + ':' + password, host: host, port: '443', timeout: 120000};
+    shopify._api = new $.ShopifyApi(opts);
   }
 
   return shopify._api;
@@ -47,10 +42,9 @@ shopify._getApi = function (apiKey, password, host) {
  * @param {string}
  * @return {string}
  */
-shopify._makeAssetKey = function (filepath, base) {
-  filepath = shopify._makePathRelative(filepath, base);
-
-  return encodeURI(filepath);
+shopify._makeAssetKey = function(filepath, base) {
+  var path = shopify._makePathRelative(filepath, base);
+  return encodeURI(path);
 };
 
 /*
@@ -58,11 +52,10 @@ shopify._makeAssetKey = function (filepath, base) {
  *
  * @return {string}
  */
-shopify._getBasePath = function (filebase) {
+shopify._getBasePath = function(filebase) {
   if (!shopify._basePath) {
     var base = filebase;
-
-    shopify._basePath = (base.length > 0) ? path.resolve(base) : process.cwd();
+    shopify._basePath = (0 < base.length) ? $.path.resolve(base) : process.cwd();
   }
 
   return shopify._basePath;
@@ -74,7 +67,7 @@ shopify._getBasePath = function (filebase) {
  * @param {string} basePath
  * @return {void}
  */
-shopify._setBasePath = function (basePath) {
+shopify._setBasePath = function(basePath) {
   shopify._basePath = basePath;
 };
 
@@ -84,12 +77,10 @@ shopify._setBasePath = function (basePath) {
  * @param {string} filepath
  * @return {string}
  */
-shopify._makePathRelative = function (filepath, base) {
+shopify._makePathRelative = function(filepath, base) {
   var basePath = shopify._getBasePath(base);
-
-  filepath = path.relative(basePath, filepath);
-
-  return filepath.replace(/\\/g, '/');
+  var relativePath = $.path.relative(basePath, filepath);
+  return relativePath.replace(/\\/g, '/');
 };
 
 /**
@@ -98,7 +89,7 @@ shopify._makePathRelative = function (filepath, base) {
  * @param {object} options
  * @return {void}
  */
-shopify._setOptions = function (options) {
+shopify._setOptions = function(options) {
   if (!options) {
     return;
   }
@@ -126,17 +117,13 @@ shopify._setOptions = function (options) {
  * @param {base} sting - options.basePath
  * @param {themeid} string - Shopify theme
  */
-shopify.upload = function (filepath, file, host, base, themeid, cb) {
+shopify.upload = function(filepath, file, host, base, themeid, cb) {
 
   var api = shopifyAPI;
   var key = shopify._makeAssetKey(filepath, base);
-  var isBinary = isBinaryFile(filepath);
+  var isBinary = $.isBinaryFile(filepath);
   var contents = file.contents;
-  var props = {
-    asset: {
-      key: key
-    }
-  };
+  var props = {asset: {key: key}};
   var filename = filepath.replace(/^.*[\\\/]/, '');
 
   if (isBinary) {
@@ -145,18 +132,18 @@ shopify.upload = function (filepath, file, host, base, themeid, cb) {
     props.asset.value = contents.toString();
   }
 
-  gutil.log(gutil.colors.gray.dim('Uploading: ' + filename));
+  $.gutil.log($.gutil.colors.gray.dim('Uploading: ' + filename));
 
-  function onUpdate(err, resp) {
-    if (err && err.type === 'ShopifyInvalidRequestError') {
-      gutil.log(gutil.colors.red('Error uploading file ' + filepath));
+  var onUpdate = function(err, resp) {
+    if (err && ('ShopifyInvalidRequestError' === err.type)) {
+      $.gutil.log($.gutil.colors.red('Error uploading file ' + filepath));
     } else if (!err) {
-      gutil.log(gutil.colors.green('Upload Complete: ' + filename));
+      $.gutil.log($.gutil.colors.green('Upload Complete: ' + filename));
     } else {
-      gutil.log(gutil.colors.red('Error undefined! ' + err.type + ' ' + err.detail));
+      $.gutil.log($.gutil.colors.red('Error undefined! ' + err.type + ' ' + err.detail));
     }
     cb();
-  }
+  };
 
   if (themeid) {
     api.asset.update(themeid, props, onUpdate);
@@ -164,7 +151,6 @@ shopify.upload = function (filepath, file, host, base, themeid, cb) {
     api.assetLegacy.update(props, onUpdate);
   }
 };
-
 
 /*
  * Remove a given file path from Shopify.
@@ -177,24 +163,24 @@ shopify.upload = function (filepath, file, host, base, themeid, cb) {
  * @param {base} sting - options.basePath
  * @param {themeid} string - Shopify theme
  */
-shopify.destroy = function (filepath, file, host, base, themeid, cb) {
+shopify.destroy = function(filepath, file, host, base, themeid, cb) {
 
   var api = shopifyAPI;
   var key = shopify._makeAssetKey(filepath, base);
   var filename = filepath.replace(/^.*[\\\/]/, '');
 
-  gutil.log(gutil.colors.red.dim('Removing file: ' + filename));
+  $.gutil.log($.gutil.colors.red.dim('Removing file: ' + filename));
 
-  function onDestroy(err, resp) {
-    if (err && err.type === 'ShopifyInvalidRequestError') {
-      gutil.log(gutil.colors.red('Error removing file: ' + filepath));
+  var onDestroy = function(err, resp) {
+    if (err && ('ShopifyInvalidRequestError' === err.type)) {
+      $.gutil.log($.gutil.colors.red('Error removing file: ' + filepath));
     } else if (!err) {
-      gutil.log(gutil.colors.green('File removed: ' + filename));
+      $.gutil.log($.gutil.colors.green('File removed: ' + filename));
     } else {
-      gutil.log(gutil.colors.red('Error undefined! ' + err.type));
+      $.gutil.log($.gutil.colors.red('Error undefined! ' + err.type));
     }
     cb();
-  }
+  };
 
   if (themeid) {
     api.asset.destroy(themeid, key, onDestroy);
@@ -202,7 +188,6 @@ shopify.destroy = function (filepath, file, host, base, themeid, cb) {
     api.assetLegacy.destroy(key, onDestroy);
   }
 };
-
 
 /*
  * Public function for process deployment queue for new files added via the stream.
@@ -229,125 +214,142 @@ function gulpShopifyUpload(apiKey, password, host, themeid, options) {
   shopify._setOptions(options);
   shopifyAPI = shopify._getApi(apiKey, password, host);
 
-  if (typeof apiKey === 'undefined') {
-    throw new PluginError(PLUGIN_NAME, 'Error, API Key for shopify does not exist!');
+  if ('undefined' === typeof apiKey) {
+    throw new $.gutil.PluginError(PLUGIN_NAME, 'Error, API Key for shopify does not exist!');
   }
-  if (typeof password === 'undefined') {
-    throw new PluginError(PLUGIN_NAME, 'Error, password for shopify does not exist!');
+  if ('undefined' === typeof password) {
+    throw new $.gutil.PluginError(PLUGIN_NAME, 'Error, password for shopify does not exist!');
   }
-  if (typeof host === 'undefined') {
-    throw new PluginError(PLUGIN_NAME, 'Error, host for shopify does not exist!');
+  if ('undefined' === typeof host) {
+    throw new $.gutil.PluginError(PLUGIN_NAME, 'Error, host for shopify does not exist!');
   }
-
 
   shopifyAPI.theme.list(function(err, obj) {
     if (err || !obj.themes) {
-      gutil.log(gutil.colors.red(err));
+      $.gutil.log($.gutil.colors.red(err));
       return;
-    } else {
-      if (themeid === 'BACKDOOR') {
-        // Secret backdoor to upload to any theme on the fly
-        var themes = [];
-        obj.themes.forEach(function(theme) {
-          var t = theme.id + ' - ' + theme.name;
-          if (theme.role.length > 0) {
-            t += ' (' + theme.role + ')';
-          }
-          themes.push(t);
-        });
-        inquirer.prompt([
-          {
-            type: 'list',
-            name: 'theme',
-            message: 'Which theme would you like to use?',
-            choices: themes,
-            filter: function(val) {
-              var fullName = val.match(/(\d+) - (.*)/);
-              return {
-                id: fullName[1],
-                name: fullName[2]
-              };
-            }
-          }
-        ], function(answers) {
-          var downcasedName = answers.theme.name.toLowerCase();
-          if (/(production|staging)/.test(downcasedName)) {
-            gutil.log(gutil.colors.red('\n\nDIRECTLY UPLOADING TO A CLIENT FACING ENVIRONMENT -- CAREFUL!\n\n'));
-          }
-          themeid = answers.theme.id;
-          gutil.log(gutil.colors.gray('Connected to: ') + gutil.colors.magenta(host) + gutil.colors.gray(' theme id: ') + gutil.colors.magenta(answers.theme.id) + gutil.colors.gray(' theme name: ') + gutil.colors.magenta(answers.theme.name));
-          opn('http://'+host+'?preview_theme_id='+answers.theme.id);
-        });
-      } else {
-        // validate that the themeid passed in to see if it's an actual themeid
-        themeid = parseInt(themeid);  // convert string to int
-        var themeidValid = false;
-        var themeName;
-        obj.themes.forEach(function(theme) {
-          if (theme.id == themeid) {
-            themeidValid = true;
-            themeName = theme.name;
-          }
-        });
-        if (themeidValid) {
-          gutil.log(gutil.colors.gray('Connected to: ') + gutil.colors.magenta(host) + gutil.colors.gray(' theme id: ') + gutil.colors.magenta(themeid) + gutil.colors.gray(' theme name: ') + gutil.colors.magenta(themeName));
-          opn('http://'+host+'?preview_theme_id='+themeid);
-        } else {
-          throw new PluginError(PLUGIN_NAME, 'Error, please make sure you\'re using a real theme id');
-        }
-      }
     }
+
+    if ('BACKDOOR' === themeid) {
+      // Secret backdoor to upload to any theme on the fly
+      var themes = [];
+      obj.themes.forEach(function(theme) {
+        var t = theme.id + ' - ' + theme.name;
+
+        if (0 < theme.role.length) {
+          t += ' (' + theme.role + ')';
+        }
+
+        themes.push(t);
+      });
+
+      $.inquirer.prompt([
+        {
+          type: 'list',
+          name: 'theme',
+          message: 'Which theme would you like to use?',
+          choices: themes,
+          filter: function(val) {
+            var fullName = val.match(/(\d+) - (.*)/);
+            return {id: fullName[1], name: fullName[2]};
+          }
+        }
+      ], function(answers) {
+        var downcasedName = answers.theme.name.toLowerCase();
+
+        if (/(production|staging)/.test(downcasedName)) {
+          $.gutil.log($.gutil.colors.red('\n\nDIRECTLY UPLOADING TO A CLIENT FACING ENVIRONMENT -- CAREFUL!\n\n'));
+        }
+
+        themeid = answers.theme.id;
+        $.gutil.log($.gutil.colors.gray('Connected to: ') +
+          $.gutil.colors.magenta(host) +
+          $.gutil.colors.gray(' theme id: ') +
+          $.gutil.colors.magenta(answers.theme.id) +
+          $.gutil.colors.gray(' theme name: ') +
+          $.gutil.colors.magenta(answers.theme.name));
+        $.open('http://' + host + '?preview_theme_id=' + answers.theme.id);
+      });
+
+      return;
+    }
+
+    // validate that the themeid passed in to see if it's an actual themeid
+    themeid = parseInt(themeid);  // convert string to int
+    var themeidValid = false;
+    var themeName;
+
+    obj.themes.forEach(function(theme) {
+      if (theme.id == themeid) {
+        themeidValid = true;
+        themeName = theme.name;
+      }
+    });
+
+    if (themeidValid) {
+      $.gutil.log($.gutil.colors.gray('Connected to: ') +
+        $.gutil.colors.magenta(host) +
+        $.gutil.colors.gray(' theme id: ') +
+        $.gutil.colors.magenta(themeid) +
+        $.gutil.colors.gray(' theme name: ') +
+        $.gutil.colors.magenta(themeName));
+      $.open('http://' + host + '?preview_theme_id=' + themeid);
+    } else {
+      throw new $.gutil.PluginError(PLUGIN_NAME, 'Error, please make sure you\'re using a real theme id');
+    }
+
   });
 
   // creating a stream through which each file will pass
-  stream = through.obj(function (file, enc, cb) {
+  stream = $.through.obj(function(file, enc, cb) {
     if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+      this.emit('error', new $.gutil.PluginError(PLUGIN_NAME, 'Streams are not supported!'));
       return cb();
     }
 
     var self = this;
 
-    if (themeid == null || file.path.indexOf('.DS_Store') !== -1) {
+    if (null === themeid || file.path.indexOf('.DS_Store') !== -1) {
       self.push(file);
       cb();
-    } else {
-      if (file.isBuffer()) {
-        // deploy immediately if within the burst bucket size, otherwise queue
-        if (fileCount <= apiBurstBucketSize) {
-          shopify.upload(file.path, file, host, '', themeid, function() {
-            self.push(file);
-            cb();
-          });
-        } else {
-          // Delay deployment based on position in the array to deploy 2 files per second
-          // after hitting the initial burst bucket limit size
-          setTimeout(shopify.upload.bind(null, file.path, file, host, '', themeid, function() {
-            self.push(file);
-            cb();
-          }), ((fileCount - apiBurstBucketSize) / 2) * 1000);
-        }
-        fileCount++;
-      }
+      return;
+    }
 
-      // If file is removed locally, destroy it on Shopify
-      if (file.isNull()) {
-        // Remove immediately if within the burst bucket size, otherwise queue
-        if (fileCount <= apiBurstBucketSize) {
-          shopify.destroy(file.path, file, host, '', themeid, function(){
-            self.push(file);
-            cb();
-          });
-        } else {
-          // Delay removal based on position in the array to deploy 2 files per second
-          // after hitting the initial burst bucket limit size
-          setTimeout(shopify.destroy.bind(null, file.path, file, host, '', themeid, function() {
-            self.push(file);
-            cb();
-          }), ((fileCount - apiBurstBucketSize) / 2) * 1000);
-        }
-        fileCount++;
+    if (file.isBuffer()) {
+      // deploy immediately if within the burst bucket size, otherwise queue
+      if (fileCount <= apiBurstBucketSize) {
+        shopify.upload(file.path, file, host, '', themeid, function() {
+          self.push(file);
+          cb();
+        });
+      } else {
+        // Delay deployment based on position in the array to deploy 2 files per second
+        // after hitting the initial burst bucket limit size
+        setTimeout(shopify.upload.bind(null, file.path, file, host, '', themeid, function() {
+          self.push(file);
+          cb();
+        }), ((fileCount - apiBurstBucketSize) / 2) * 1000);
       }
+      fileCount++;
+    }
+
+    // If file is removed locally, destroy it on Shopify
+    if (file.isNull()) {
+      // Remove immediately if within the burst bucket size, otherwise queue
+      if (fileCount <= apiBurstBucketSize) {
+        shopify.destroy(file.path, file, host, '', themeid, function() {
+          self.push(file);
+          cb();
+        });
+      } else {
+        // Delay removal based on position in the array to deploy 2 files per second
+        // after hitting the initial burst bucket limit size
+        setTimeout(shopify.destroy.bind(null, file.path, file, host, '', themeid, function() {
+          self.push(file);
+          cb();
+        }), ((fileCount - apiBurstBucketSize) / 2) * 1000);
+      }
+      fileCount++;
     }
 
   });
